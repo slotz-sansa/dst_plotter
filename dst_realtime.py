70127193b70796bf3afaffbcd763e651d89444b1
slotz-sansa <slotz@sansa.org.za>
2025-11-13 12:50:39 +0200

    Moved dst file reading and processing to own function.

--- dst_realtime.py
+++ dst_realtime.py
@@ -28,41 +28,7 @@ def main():
 
     getfile(url=dst_url + dst_filename, local_path=dst_filename)
 
-    with open(dst_filename, "r") as file:
-        lines = file.readlines()
-
-    date_str = []
-    data_str = []
-    data_list = []
-    df_list = []
-
-    for line in lines:
-        line = line.strip()
-        if len(line)==120:
-            date_str.append(line[3:7] + line[8:10])
-            data_str.append(line[20:-4])
-
-    print(len(data_str))
-
-    data_matrix = np.zeros((len(data_str),hours_perday))
-
-
-    for i,text in enumerate(data_str):
-        df = pd.DataFrame()
-        # chunks = textwrap(data,4)
-        chunks = np.array([int(text[j:j+number_length]) for j in range(0, len(text), number_length)])
-        # print(chunks.shape)
-        data_matrix[i,:] = chunks
-        start_time = dt.datetime.strptime('20'+date_str[i], '%Y%m%d')
-        df['pdate'] = pd.date_range(start=start_time, periods=hours_perday, freq='1H')
-        df['dst'] = chunks
-        df_list.append(df)
-
-        # data_list.append([int(text[j:j+k]) for j in range(0, len(text), k)])
-
-    df = pd.concat(df_list)
-    df.set_index('pdate',inplace=True)
-    df.dst.mask(df.dst==na_val, inplace=True)
+    df = processDstFile(dst_filepath=dst_filename)
 
     # make plot
     plt.figure()
@@ -90,3 +56,36 @@ def getfile(url, local_path):
     print(f"File saved to {local_path}")
 
     return 
+
+
+def processDstFile(dst_filepath):
+
+    with open(dst_filepath, "r") as file:
+        lines = file.readlines()
+
+    date_str = []
+    data_str = []
+    df_list = []
+
+    for line in lines:
+        line = line.strip()
+        if len(line)==120:
+            date_str.append(line[3:7] + line[8:10])
+            data_str.append(line[20:-4])
+
+    data_matrix = np.zeros((len(data_str),hours_perday))
+
+    for i,text in enumerate(data_str):
+        df = pd.DataFrame()
+        chunks = np.array([int(text[j:j+number_length]) for j in range(0, len(text), number_length)])
+        data_matrix[i,:] = chunks
+        start_time = dt.datetime.strptime('20'+date_str[i], '%Y%m%d')
+        df['pdate'] = pd.date_range(start=start_time, periods=hours_perday, freq='1H')
+        df['dst'] = chunks
+        df_list.append(df)
+
+    df = pd.concat(df_list)
+    df.set_index('pdate',inplace=True)
+    df.dst.mask(df.dst==na_val, inplace=True)
+
+    return df 
